08946f7908f7d4d8d417a2c4a0e494464dd70f45
Tim Teasdale <tim.teasdale@gmail.com>
2015-11-28 12:52:20 +0000

    testing adding routes for topic and area

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/domain/:topic', function(req, res, next) {
+  res.render('domain', { title: req.params.topic + " page", topic: req.params.topic });
+});
+
+
+router.get('/game/:topic/:area', function(req, res, next) {
+  res.render('domain', { title: req.params.topic + " " + req.params.area + " games page", topic: req.params.topic, area: req.params.area });
+});
+
 router.post('/post', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
