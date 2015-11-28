96a2c550c4d66dc418975d4c9508d0c2025b732a
Nicolas Deslandes <ndeslandes@live.fr>
2015-11-28 15:42:10 +0000

    updated multiple choice game

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,43 @@ router.get('/game/:topic/:area', function(req, res, next) {
   res.render('game', { title: req.params.topic + " " + req.params.area + " games page", topic: req.params.topic, area: req.params.area });
 });
 
+var runMultipleChoiceGame = function(req, res, isPost) {
+  var sentences = [{
+    id: 0,
+    beginning: 'Today I met Henry on the tennis',
+    choices: ['court', 'pitch', 'field'],
+    end: 'for a friendly game.'}];
+
+    var gameType = 'mc'
+    var success = null
+    if (isPost){
+      var answer = req.body.answer;
+      var testId = req.body.testId;
+      console.log("Received input: " + answer)
+      console.log("Received input: " + testId)
+      success = (answer === 'court')
+    }
+    res.render(gameType, {
+       title: req.params.topic + " " + req.params.area + " games page from custom page",
+       topic: req.params.topic,
+       area: req.params.area,
+       test: sentences[0],
+       success: success
+     });
+};
+
+router.get('/game/:topic/:area/mc', function(req, res, next) {
+    runMultipleChoiceGame(req, res, false);
+});
+router.post('/game/:topic/:area/mc', function(req, res, next) {
+    runMultipleChoiceGame(req, res, true);
+});
+
 router.get('/game/:topic/:area/:gameType', function(req, res, next) {
   var gameType = req.params.gameType
   res.render(gameType, { title: req.params.topic + " " + req.params.area + " games page", topic: req.params.topic, area: req.params.area });
 });
+
 
 router.post('/post', function(req, res, next) {
   res.render('index', { title: 'Express' });
